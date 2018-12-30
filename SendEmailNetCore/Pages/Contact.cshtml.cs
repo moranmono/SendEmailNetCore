055ec52a3aa9325820a4bd01ccd9b62bb7e7b447
Moran Monovich <moranm@meitavdash.co.il>
2018-12-30 14:39:24 +0200

    Creating contact page functionlty

--- SendEmailNetCore/Pages/Contact.cshtml.cs
+++ SendEmailNetCore/Pages/Contact.cshtml.cs
@@ -1,17 +1,48 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SendEmailNetCore.Pages
 {
     public class ContactModel : PageModel
     {
-        public void OnGet()
-        {
+        public string PostedMessage { get; set; } = string.Empty;
 
+        public void OnGet(int id)
+        {
+            ViewData["PostedMessage"] = "Your message has been sent [viewdata]";
+            PostedMessage = "Your message has been sent [property]";
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            return Redirect("/contact");
+        }
+
+        public void SendMail()
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("moran mono", "moran@mono.com"));
+            message.To.Add(new MailboxAddress("boris moris", "moris@belhasan.com"));
+            message.Subject = "what is up?";
+            message.Body = new TextPart("plain")
+            {
+                Text = "Send email app dot net core 2.2"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("username", "password");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+	
     }
 }
